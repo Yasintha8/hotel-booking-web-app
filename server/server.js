2da10b8132dcaff02280445d8c4adfc4dbeb2793
Yasintha28 <yasinthachamikara28@gmail.com>
2025-06-22 14:01:49 +0530

    install packages

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import "dotenv/config";
+import cors from 'cors';
+
+const app = express();
+app.use(cors()); //Enable Cross-origin Resource Sharing
+
+app.get('/',(req, res)=> res.send('API is working!'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
