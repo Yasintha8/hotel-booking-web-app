8a67b0f10be66ba093169fd94fedb2af840982ee
Yasintha28 <yasinthachamikara28@gmail.com>
2025-07-09 00:13:51 +0530

    Add hotel registration feature with model, routes and controller

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import clerkwebhooks from './controllers/clerkWebhooks.js';
 import userRouter from './routes/userRoutes.js';
+import hotelRouter from './routes/hotelRoutes.js';
 
 connectDB()
 
@@ -20,6 +21,7 @@ app.use("/api/clerk", clerkwebhooks)
 
 app.get('/',(req, res)=> res.send('API is working!'));
 app.use('/api/user', userRouter)
+app.use('/api/hotels', hotelRouter)
 
 const PORT = process.env.PORT || 3000;
 
