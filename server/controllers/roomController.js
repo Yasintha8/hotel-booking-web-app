1e460a3d397563df08760bfa217511e86ce852f2
Yasintha28 <yasinthachamikara28@gmail.com>
2025-07-09 07:42:44 +0530

    Add Cloudinary configuration and room model with controller for hotel room management

---
+++ server/controllers/roomController.js
@@ -0,0 +1,41 @@
+import Hotel from '../models/hotel.js';
+
+// API to create a new room for a hotel
+
+export const createRoom = async (req, res) => {
+    try {
+        const { roomType, pricePerNight, amenities } = req.body;
+        const hotel = await Hotel.findOne({owner: req.auth.userId});
+
+        if(!hotel) 
+            return res.json({
+                success: false,
+                message: "No Hotel Found"
+            });
+        
+    } catch (error) {
+        
+    }
+}
+
+
+// API to get all rooms
+
+export const getRooms = async (req, res) => {
+    
+}
+
+// API to get all rooms for a specific hotel
+
+export const getOwnerRooms = async (req, res) => {
+    
+}
+
+
+// API to toggle availability for a room
+
+export const toogleRoomAvability = async (req, res) => {
+    
+}
+
+
