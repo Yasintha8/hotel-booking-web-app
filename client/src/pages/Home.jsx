8c98aabae610a511f6943b0eedd2a884d8b9da5b
Yasintha28 <yasinthachamikara28@gmail.com>
2025-07-15 08:19:51 +0530

    Add RecommendedHotels component to display filtered hotel recommendations based on searched cities

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOffers from '../components/ExclusiveOffers'
 import Testimonials from '../components/Testimonials'
 import NewsLetter from '../components/NewsLetter'
+import RecommendedHotels from '../components/RecommendedHotels'
 
 const Home = () => {
   return (
     <>
         <Hero />
+        <RecommendedHotels />
         <FeaturedDestination />
         <ExclusiveOffers />
         <Testimonials />
