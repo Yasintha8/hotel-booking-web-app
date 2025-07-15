de4b2a4bdfe9d9639789ee5c8e6f5034d0daad46
Yasintha28 <yasinthachamikara28@gmail.com>
2025-07-15 17:28:41 +0530

    Update cities list in assets and add 'Double Bed' option in AddRoom component

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -75,10 +75,10 @@ export const assets = {
 }
 
 export const cities = [
-    "Dubai",
-    "Singapore",
-    "New York",
-    "London",
+    "Colombo 03",
+    "Galle",
+    "Kandy",
+    "Negombo",
 ];
 
 // Exclusive Offers Dummy Data
