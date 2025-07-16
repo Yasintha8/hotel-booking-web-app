05405e3fd7e09d5a0fc59fea9d2835bef250a307
Yasintha28 <yasinthachamikara28@gmail.com>
2025-07-16 19:22:17 +0530

    Fix username assignment logic in clerk webhooks to prioritize provided username over concatenated names

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -25,7 +25,7 @@ const clerkwebhooks = async (req, res) => {
                 const userData = {
                     _id: data.id,
                     email: data.email_addresses[0].email_address,
-                    username: data.first_name + " " + data.last_name,
+                    username: data.username || (data.first_name + " " + data.last_name),
                     image: data.image_url,
                 }
                 await User.create(userData);
@@ -36,7 +36,7 @@ const clerkwebhooks = async (req, res) => {
                 const userData = {
                     _id: data.id,
                     email: data.email_addresses[0].email_address,
-                    username: data.first_name + " " + data.last_name,
+                    username: data.username || (data.first_name + " " + data.last_name),
                     image: data.image_url,
                 }
                 await User.findByIdAndUpdate(data.id, userData);
